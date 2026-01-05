436a6e140d66e7e133f4a536b0fd9e5bf22427bf
heoeunseo <heoeunseong140@gmail.com>
2026-01-05 23:34:18 +0900

    design : "텍스트 스타일 시스템 정의"
    Figma 타이포그래피 가이드 기준
    title-sm, title-Bold-16, title-sm-14, text-medium, caption 텍스트 스타일 토큰 추가
    
    fix: #4

---
+++ lib/core/design_system/app_text_styles.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  static const TextStyle titleSm = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    height: 34,
+  );
+
+  static const TextStyle titleBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    height: 22,
+  );
+
+  static const TextStyle titleBold14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    height: 20,
+  );
+
+  static const TextStyle textMedium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    height: 20,
+  );
+
+  static const TextStyle cpation = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    height: 17,
+  );
+
+}

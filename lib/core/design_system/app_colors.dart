ac589a735da16232f73d1506f1786ccd0a74dde6
heoeunseo08 <s24053@gsm.hs.kr>
2026-01-05 21:09:57 +0900

    design : "컬러 시스템 디자인 토큰 정의"
    Figma 컬러 가이드 기준
    primary, action, grayscale 컬러 토큰 추가
    
    fix: #4

---
+++ lib/core/design_system/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  AppColors._();
+
+  //Primary color(brown)
+  static const Color primary = Color(0xff6D3300);
+  static const Color primaryLight = Color(0xffDBCDBF);
+  static const Color primaryDark = Color(0xff482506);
+  static const Color background = Color(0xffFFFBF6);
+
+  //Action color
+  static const Color yellow = Color(0xffFFCE3D);
+  static const Color redLight = Color(0xffFFDEDE);
+  static const Color red = Color(0xffFC624A);
+
+  //Gray scale color
+  static const Color white = Color(0xffFFFFFF);
+  static const Color Gray10 = Color(0xffC2C1C1);
+  static const Color Gray30 = Color(0xff999797);
+  static const Color Gray50 = Color(0xff707070);
+  static const Color Gray70 = Color(0xff555555);
+  static const Color Gray80 = Color(0xff2F3233);
+  static const Color Gray90 = Color(0xff2F3233);
+  static const Color balck = Color(0xff000000);
+}

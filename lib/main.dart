2141cc05b3dd4679fc81726dd6651482f8daf99a
heoeunseo08 <s24053@gsm.hs.kr>
2026-01-09 23:40:19 +0900

    chore: main.dart merge conflict 정리
    - main.dart에서 develop 브랜치와의 import 충돌 해결
    - app_theme, app_router import 병합
    
    resolve: #15

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:ondo/core/design_system/app_colors.dart';
+import 'package:ondo/core/design_system/app_theme.dart';
 import 'package:ondo/core/router/app_router.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,44 +12,12 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This components is theroot of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.appTheme(),
       routerConfig: appRouter,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.white,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Center(
-          child: Column(
-            children: [
-              Text('ONDO'),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }

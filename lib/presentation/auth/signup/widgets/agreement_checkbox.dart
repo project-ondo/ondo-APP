01d49782ea1aa78e9ea5adc34d15137ebde8fc72
heoeunseo08 <s24053@gsm.hs.kr>
2026-01-13 19:24:42 +0900

    refactor : "checkbox - AppGap 적용"
    - agreement_checkbox.dart에 AppGap.v6 적용
    
    Resolves: #27

--- lib/presentation/auth/signup/widgets/agreement_checkbox.dart
+++ lib/presentation/auth/signup/widgets/agreement_checkbox.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ondo/core/design_system/app_colors.dart';
 import 'package:ondo/core/design_system/app_icon.dart';
+import 'package:ondo/core/design_system/app_layout.dart';
 import 'package:ondo/core/design_system/app_text_styles.dart';
 
 class AgreementCheckbox extends StatelessWidget {
@@ -24,7 +25,7 @@ class AgreementCheckbox extends StatelessWidget {
           SvgPicture.asset(
             isChecked ? AppIcon.checkOn.path : AppIcon.check.path,
           ),
-          const SizedBox(width: 6),
+          AppGap.v6,
           Text(
             '[개인정보 수집 및 이용 동의] (필수)',
             style: AppTextStyles.textMedium(

f468e1179cdcd3a11c8bcf3926442684d01d5a7c
heoeunseo08 <s24053@gsm.hs.kr>
2026-01-13 15:29:01 +0900

    refactor : "이용약관 상태 관리를 GetX controller로 분리"
    - 이용약관 동의 상태를 GetX 기반 controller로 분리
    - StatefulWidget 및 setState 제거
    - 화면은 상태 조회 및 이벤트 전달 역할만 담당
    리
    
    Resolves: #27

--- lib/presentation/auth/signup/screens/terms_agreement_screen.dart
+++ lib/presentation/auth/signup/screens/terms_agreement_screen.dart
@@ -1,10 +1,10 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:ondo/core/design_system/app_layout.dart';
-import 'package:ondo/core/design_system/component_variants.dart';
-import 'package:ondo/core/design_system/components/custom_button.dart';
 import 'package:ondo/core/ui/base/base_scaffold.dart';
+import 'package:ondo/presentation/auth/signup/controllers/terms_agreement_controller.dart';
 import 'package:ondo/presentation/auth/signup/widgets/agreement_checkbox.dart';
 import 'package:ondo/presentation/auth/signup/widgets/login_back_button.dart';
 import 'package:ondo/presentation/auth/signup/widgets/next_screen_button.dart';
@@ -15,16 +15,11 @@ void main() {
   runApp(MaterialApp(home: TermsAgreementScreen()));
 }
 
-class TermsAgreementScreen extends StatefulWidget {
-  const TermsAgreementScreen({super.key});
+class TermsAgreementScreen extends StatelessWidget {
+   TermsAgreementScreen({super.key});
 
-  @override
-  State<TermsAgreementScreen> createState() => _TermsAgreementScreenState();
-}
+  final TermsAgreementController termsAgreementController = Get.put(TermsAgreementController());
 
-bool isAgreementChecked = false;
-
-class _TermsAgreementScreenState extends State<TermsAgreementScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -34,54 +29,43 @@ class _TermsAgreementScreenState extends State<TermsAgreementScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-            Column(
-            children: [
-            AppGap.v24,
-            LoginBackButton(
-              onTap: () {
-                log('로그인으로 이동');
-              },
-            ),
+              Column(
+                children: [
+                  AppGap.v24,
+                  LoginBackButton(
+                    onTap: () {
+                      log('로그인으로 이동');
+                    },
+                  ),
 
-            SizedBox(height: 36),
-            TitleText.titleText(
-              '온도에 오신 것을 환영합니다! \n개인정보 수집 및 이용에 동의해주세요',
-            ),
+                  SizedBox(height: 36),
+                  TitleText.titleText(
+                    '온도에 오신 것을 환영합니다! \n개인정보 수집 및 이용에 동의해주세요',
+                  ),
 
-            AppGap.v24,
-            TermsAgreementCard.termsAgreementCard,
+                  AppGap.v24,
+                  TermsAgreementCard.termsAgreementCard,
 
-            AppGap.v16,
-            //check box
-            AgreementCheckbox(
-              isChecked: isAgreementChecked,
-              onTap: () {
-                log('check');
-                setState(() {
-                  isAgreementChecked = !isAgreementChecked;
-                });
-              },
-            ),
-            ],
-          ),
-
-          Column(
-            children: [
-              CustomButton(
-                text: '다음',
-                variant: ButtonVariant.primary,
-                enabled: isAgreementChecked,
-                onPressed: () {
-                  log('이동');
-                },
+                  AppGap.v16,
+                  //check box
+                  Obx(
+                    () => AgreementCheckbox(
+                      isChecked: termsAgreementController.isAgreementChecked.value,
+                      onTap: termsAgreementController.toggleAgreement,
+                    ),
+                  ),
+                ],
               ),
-              AppGap.v16,
+
+              Obx(
+                () => NextScreenButton(
+                  isAgreementChecked: termsAgreementController.canProceed,
+                ),
+              ),
             ],
           ),
-          NextScreenButton(isAgreementChecked: isAgreementChecked,),
-          ],
         ),
       ),
-    ),);
+    );
   }
 }

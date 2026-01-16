8135c4b4f0cd3c44dc625c098d0496c9dd069795
suehng0823 <s25010@gsm.hs.kr>
2026-01-17 08:29:36 +0900

    splash 화면 구현

---
+++ lib/presentation/login/screens/splash.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:ondo/core/ui/base/base_scaffold.dart';
+
+
+class Splash extends StatelessWidget {
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseScaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [Image.asset('assets/image/logo.png')],
+        ),
+      ),
+    );
+  }
+}

47ef05864cb5f4c17a88e0fa5a08698407068b04
eum018-admin <s25013@gsm.hs.kr>
2026-01-17 15:44:45 +0900

    feature : 홈화면 게시물 위젯 리펙토링 body : home_post_item.dart 파일에     - 게시물 표시 영역 별 클래스로 분리     - 정적인 크기 제한 삭제 fix : #31

--- lib/presentation/home/widgets/home_post_item.dart
+++ lib/presentation/home/widgets/home_post_item.dart
@@ -25,7 +25,6 @@ class HomePostItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 182,
       padding: AppPadding.card,
       decoration: BoxDecoration(
         borderRadius: AppRadius.baseRadius,
@@ -35,48 +34,98 @@ class HomePostItem extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           //분야 표시 영역
-          Row(
-            spacing: AppSpacing.s12,
-            children: List.generate(skills.length, (index) {
-              return Text(
-                skills[index],
-                style: AppTextStyles.caption(textColor: AppColors.gray60),
-              );
-            }),
-          ),
+          _SkillList(skills: skills),
 
           AppGap.v12,
 
-          //제목 영역
-          Text(
-            title,
-            style: AppTextStyles.titleSm14(),
-          ),
-
-          //작성자 작성자 표시 영역
-          Text(
-            author,
-            style: AppTextStyles.caption(),
-          ),
+          //제목 + 작성자 표시 영역
+          _MainContent(title: title, author: author),
 
           AppGap.v16,
 
           //하단 북마크, 좋아요 + 생성 시간 표시 영역
-          Row(
-            spacing: AppSpacing.s8,
-            children: [
-              customIcon(AppIcon.heart.path, favorites),
-              customIcon(AppIcon.bookmark.path, bookmarks),
-              Expanded(
-                child: Text(
-                  "${createMinutes}분전",
-                  style: AppTextStyles.caption(textColor: AppColors.gray50),
-                ),
-              ),
-            ],
+          _SubContent(
+            favorites: favorites,
+            bookmarks: bookmarks,
+            createMinutes: createMinutes,
           ),
         ],
       ),
+    );
+  }
+}
+
+class _SkillList extends StatelessWidget {
+  final List<String> skills;
+
+  const _SkillList({required this.skills});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: AppSpacing.s12,
+      children: List.generate(skills.length, (index) {
+        return Text(
+          skills[index],
+          style: AppTextStyles.caption(textColor: AppColors.gray60),
+        );
+      }),
+    );
+  }
+}
+
+class _MainContent extends StatelessWidget {
+  final String title;
+  final String author;
+
+  const _MainContent({super.key, required this.title, required this.author});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //제목 영역
+        Text(
+          title,
+          style: AppTextStyles.titleSm14(),
+        ),
+
+        //작성자 작성자 표시 영역
+        Text(
+          author,
+          style: AppTextStyles.caption(),
+        ),
+      ],
+    );
+  }
+}
+
+class _SubContent extends StatelessWidget {
+  final int favorites;
+  final int bookmarks;
+  final int createMinutes;
+
+  const _SubContent({
+    super.key,
+    required this.favorites,
+    required this.bookmarks,
+    required this.createMinutes,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: AppSpacing.s8,
+      children: [
+        customIcon(AppIcon.heart.path, favorites),
+        customIcon(AppIcon.bookmark.path, bookmarks),
+        Expanded(
+          child: Text(
+            "${createMinutes}분전",
+            style: AppTextStyles.caption(textColor: AppColors.gray50),
+          ),
+        ),
+      ],
     );
   }
 

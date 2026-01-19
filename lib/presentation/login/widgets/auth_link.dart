319b06ae09de2f0c24e96bf11cad1126a9c98be5
suehng0823 <s25010@gsm.hs.kr>
2026-01-19 19:14:27 +0900

    feature : 유효성 검사 구현

--- lib/presentation/login/widgets/auth_link.dart
+++ lib/presentation/login/widgets/auth_link.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ondo/core/design_system/app_colors.dart';
 import 'package:ondo/core/design_system/app_text_styles.dart';
 
-class AuthLink extends StatelessWidget { // 비밀번호 찾기 | 회원가입 텍스트 버튼
+class AuthLink extends StatelessWidget {
   const AuthLink({super.key});
 
   @override

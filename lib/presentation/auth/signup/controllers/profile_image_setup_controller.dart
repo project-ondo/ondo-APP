2e24cfd2bb44da0f0f78710a81446dda8d701073
heoeunseo <heoeunseong140@gmail.com>
2026-01-22 02:03:18 +0900

    refactor : 프로필 이미지 설정 컨트롤러 역할 분리
    Controller에서 Widget 의존성을 제거하고
    프로필 이미지 상태를 데이터 중심으로 재구성
    
    Fix: #48

--- lib/presentation/auth/signup/controllers/profile_image_setup_controller.dart
+++ lib/presentation/auth/signup/controllers/profile_image_setup_controller.dart
@@ -1,39 +1,40 @@
 import 'dart:developer';
 
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:ondo/core/design_system/app_icon.dart';
 
-class ProfileImageSetupController extends GetxController{
-  late Object profileImage;
+class ProfileImageSetupController extends GetxController {
+  String? profileImagePath;
 
   final ImagePicker _picker = ImagePicker();
+
+  bool get isDefaultProfile => profileImagePath == null;
 
   @override
   void onInit() {
     super.onInit();
-    _setDefaultProfile();
+    resetDefaultProfile();
   }
 
-  void _setDefaultProfile() {
-    profileImage = SvgPicture.asset(AppIcon.defaultProfile.path);
+  void resetDefaultProfile() {
+    profileImagePath = null;
     update();
   }
 
-  Future<void> pickFromGallery() async{
+  Future<void> pickFromGallery() async {
     final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
 
-    if(image != null){
-      profileImage = image;
+    if (image != null) {
+      profileImagePath = image.path;
       update();
     }
   }
 
-  void applyDefaultProfile(){
-    _setDefaultProfile();
-  }
-  void submitProfileImage(){
-    log('프로필 이미지 설정완료: $profileImage');
+  void submitProfileImage() {
+    log(
+      isDefaultProfile
+          ? '프로필 이미지 설정완료: 기본 프로필'
+          : '프로필 이미지 설정완료: $profileImagePath',
+    );
   }
 }

0ed687bde4ad1018984c065300995fa970099a23
heoeunseo08 <s24053@gsm.hs.kr>
2026-01-28 23:18:03 +0900

    feature : "회원가입 플로우 정보 제출 메서드 추가"
    회원가입 과정에서 입력된 이메일, 비밀번호, 닉네임, 전공, 관심분야를
    하나의 컨트롤러에서 취합하고 테스트용 submit 진입점을 추가함
    
    Fix: #55

--- lib/presentation/auth/signup/controllers/signup_flow_controller.dart
+++ lib/presentation/auth/signup/controllers/signup_flow_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:get/get.dart';
 
 class SignupFlowController extends GetxController {
@@ -34,6 +36,15 @@ class SignupFlowController extends GetxController {
       introduction != null &&
       major != null &&
       interests.isNotEmpty;
+
+  void submitInfo(){
+    //현재는 디버그 용 코드 서버 연결 후 서버에 값을 보내는 역할 예정
+    log('email: $email');
+    log('password: $password');
+    log('nickname: $nickname');
+    log('introduction: $introduction');
+    log('interests: $interests');
+  }
 
   //회원가입 설정 값 초기화
   void clear() {

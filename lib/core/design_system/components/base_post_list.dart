cfb2369274989c9e14079d452f131c777cbcd6ca
eum018-admin <s25013@gsm.hs.kr>
2026-01-28 16:07:52 +0900

    feature : 게시물 리스트 및 게시물 카드 리펙토링 body : base_post_list.dart 파일     - floor 추가, 리스트 크기를 동적으로 관리     - post_item.dart의 PostItem을 같이 관리       - 이후에 서버 모델이 정해지면 List<Widget>이 아닌 List<Model>로 정의하여 관리 fix : #51

--- lib/core/design_system/components/base_post_list.dart
+++ lib/core/design_system/components/base_post_list.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:ondo/core/design_system/app_colors.dart';
 import 'package:ondo/core/design_system/app_layout.dart';
 import 'package:ondo/core/design_system/app_text_styles.dart';
-import 'package:ondo/core/design_system/components/post_item.dart';
+
+import '../app_icon.dart';
 
 @immutable
 abstract class BasePostList extends StatelessWidget {
   final String title;
+  final int floor;
+  final double itemHeight;
 
-  const BasePostList({super.key, required this.title});
+
+  const BasePostList({super.key, required this.title, this.floor = 2, this.itemHeight = 143});
 
   List<Widget> list();
 
@@ -17,66 +21,179 @@ abstract class BasePostList extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        _Title(title: title),
+        _title(),
 
         AppGap.v16,
 
         ConstrainedBox(
-          constraints: const BoxConstraints(
-            maxHeight: 270,
-            maxWidth: 380,
+          constraints: BoxConstraints(
+            maxHeight: (floor * itemHeight) - 16,
+            maxWidth: double.maxFinite,
           ),
-          child: _PostList(list: list()),
+          child: _postList(),
         ),
       ],
     );
   }
-}
 
-@immutable
-class _Title extends StatelessWidget {
-  final String title;
-
-  const _Title({required this.title});
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _title() {
     return Text(
       title,
       style: AppTextStyles.titleSm16(textColor: AppColors.gray90),
     );
   }
+
+  final double _itemSpace = AppSpacing.s16;
+
+  Widget _postList() {
+    final int pageTotal = list().length ~/ (floor * 2);
+    print(pageTotal);
+
+    return PageView.builder(
+      itemBuilder: (context, pageIndex) {
+        int startItemIndex = pageIndex * (floor * 2);
+
+        int endItemIndex = startItemIndex + (floor * 2);
+
+        return GridView(
+          scrollDirection: Axis.horizontal,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: _itemSpace,
+            crossAxisSpacing: _itemSpace,
+            childAspectRatio: 0.7,
+          ),
+          children: List.of(
+            list().sublist(
+              startItemIndex,
+              startItemIndex +
+                  (endItemIndex > list().length
+                      ? list().length % (pageTotal * (floor * 2))
+                      : (floor * 2)),
+            ),
+          ),
+        );
+      },
+      itemCount: pageTotal + 1,
+    );
+  }
 }
 
-class _PostList extends StatelessWidget {
-  final List<Widget> list;
+class PostItem extends StatelessWidget {
+  final List<String> skills;
+  final String title;
+  final String author;
+  final int favorites;
+  final int bookmarks;
+  final int createMinutes;
 
-  const _PostList({required this.list});
-
-  final double _itemSpace = 16;
+  const PostItem({
+    super.key,
+    required this.skills,
+    required this.title,
+    required this.author,
+    required this.bookmarks,
+    required this.favorites,
+    required this.createMinutes,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      scrollDirection: Axis.horizontal,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        mainAxisSpacing: _itemSpace,
-        crossAxisSpacing: _itemSpace,
-        childAspectRatio: 0.7,
+    return Container(
+      padding: AppPadding.card,
+      decoration: BoxDecoration(
+        borderRadius: AppRadius.baseRadius,
+        color: AppColors.white,
       ),
-      children: List.generate(4, (index) {
-        return index > list.length
-            ? SizedBox.shrink()
-            : PostItem(
-                skills: ['UI/UX', 'FrontEnd'],
-                title: "요즘 UI UX",
-                author: "김유찬",
-                bookmarks: 12,
-                favorites: 12,
-                createMinutes: 4,
-              );
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //분야 표시 영역
+          _skillList(),
+          AppGap.v12,
+
+          //제목 + 작성자 표시 영역
+          _content(),
+          AppGap.v16,
+
+          //하단 북마크, 좋아요 + 생성 시간 표시 영역
+          _sunContent(),
+        ],
+      ),
+    );
+  }
+
+  final double _skillSpacing = AppSpacing.s16;
+
+  Widget _skillList() {
+    return Row(
+      spacing: _skillSpacing,
+      children: List.generate(skills.length, (index) {
+        return Row(
+          children: [
+            Text(
+              skills[index],
+              style: AppTextStyles.caption(textColor: AppColors.gray60),
+            ),
+          ],
+        );
       }),
+    );
+  }
+
+  Widget _content() {
+    return Column(
+      children: [
+        //제목 영역
+        Text(
+          title,
+          style: AppTextStyles.titleSm14(),
+        ),
+
+        //작성자 작성자 표시 영역
+        Text(
+          author,
+          style: AppTextStyles.caption(),
+        ),
+      ],
+    );
+  }
+
+  Widget _sunContent() {
+    return Row(
+      children: [
+        _customIcon(AppIcon.heart.path, favorites),
+
+        AppGap.h8,
+
+        _customIcon(AppIcon.bookmark.path, bookmarks),
+
+        AppGap.h8,
+
+        Expanded(
+          child: Text(
+            "$createMinutes분전",
+            style: AppTextStyles.caption(textColor: AppColors.gray50),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _customIcon(String iconPath, int total) {
+    final double iconSize = 16;
+    return Row(
+      children: [
+        Image.asset(
+          iconPath,
+          height: iconSize,
+          width: iconSize,
+        ),
+        Text(
+          "$total",
+          style: AppTextStyles.caption(textColor: AppColors.gray60),
+        ),
+      ],
     );
   }
 }

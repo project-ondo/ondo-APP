332c6ea73cf30be2a6fd70bf5561d2b53e195a40
user <s25013@gsm.hs.kr>
2026-02-16 23:50:44 +0900

    feature : 위젯 나누기
    - alert_screen.dart의 알림 위젯 따로 정의
        - report_alert_card.dart
        - alert_card.dart
    - _alertPage함수를 alert_screen.dart 내의 _위젯으로 따로 관리
    
    Fix : #59

--- lib/presentation/alert/screens/alert_screen.dart
+++ lib/presentation/alert/screens/alert_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:ondo/core/design_system/app_colors.dart';
 import 'package:ondo/core/design_system/app_icon.dart';
@@ -7,6 +6,15 @@ import 'package:ondo/core/design_system/app_layout.dart';
 import 'package:ondo/core/design_system/app_text_styles.dart';
 import 'package:ondo/core/design_system/components/top_bar/controllers/main_top_bar_alert_controller.dart';
 import 'package:ondo/core/ui/base/base_scaffold.dart';
+
+import '../widgets/alert_card.dart';
+import '../widgets/report_alert_card.dart';
+
+
+void main () {
+  runApp(MaterialApp(home: AlertScreen(),));
+}
+
 
 
 @immutable
@@ -22,15 +30,6 @@ class _AlertScreenState extends State<AlertScreen> {
     MainTopBarAlertController(),
   );
 
-  final List alerts = List.filled(19, null);
-
-  ValueNotifier<int> curPageIndex = ValueNotifier(0);
-
-  bool isOverInList(int lastIndex) => lastIndex > alerts.length;
-
-  int lastIndexInList(int startIndex) =>
-      isOverInList(startIndex + 11) ? alerts.length : 11;
-
   @override
   Widget build(BuildContext context) {
     return BaseScaffold(
@@ -40,9 +39,7 @@ class _AlertScreenState extends State<AlertScreen> {
           children: [
             _title(),
             AppGap.v16,
-            _alertPage(),
-            AppGap.v16,
-            _indicator(),
+            _AlertPageList(),
           ],
         ),
       ),
@@ -62,43 +59,69 @@ class _AlertScreenState extends State<AlertScreen> {
       ),
     ],
   );
+}
 
-  Widget _alertPage() => SizedBox(
-    height: 624,
-    child: PageView.builder(
-      onPageChanged: (value) => curPageIndex.value = value,
-      itemBuilder: (context, index) {
-        return _alertList(
-          alerts.sublist(
-            index * 11,
-            lastIndexInList(index * 11),
+@immutable
+class _AlertPageList extends StatefulWidget {
+  const _AlertPageList();
+
+  @override
+  State<_AlertPageList> createState() => _AlertPageListState();
+}
+
+class _AlertPageListState extends State<_AlertPageList> {
+  final List alerts = List.filled(19, null);
+
+  ValueNotifier<int> curPageIndex = ValueNotifier(0);
+
+  bool isOverInList(int lastIndex) => lastIndex > alerts.length;
+
+  int lastIndexInList(int startIndex) =>
+      isOverInList(startIndex + 11) ? alerts.length : 11;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 624,
+          child: PageView.builder(
+            onPageChanged: (value) => curPageIndex.value = value,
+            itemBuilder: (context, index) {
+              return _alertList(
+                alerts.sublist(
+                  index * 11,
+                  lastIndexInList(index * 11),
+                ),
+              );
+            },
+            itemCount: (alerts.length / 11).ceil(),
           ),
-        );
-      },
-      itemCount: (alerts.length / 11).ceil(),
-    ),
-  );
+        ),
+        AppGap.v16,
+        _indicator(),
+      ],
+    );
+  }
 
   Widget _alertList(List subAlerts) => ListView.separated(
     itemBuilder: (context, index) {
       if (index % 2 == 0) {
-        return _ReportAlertItem(
+        return ReportAlertCard(
           profileImage: AppIcon.defaultProfile.path,
           reason: "욕설",
           restrictAt: DateTime.now(),
           restrictDuration: Duration(days: 7),
         );
       }
-      return _AlertItem(
+      return AlertCard(
         profileImage: AppIcon.defaultProfile.path,
         alertType: "누군가가 댓글을 남겼어요.",
         sendAt: Duration(hours: 3),
         comment: "진짜 ㄹㅇ...",
       );
     },
-    separatorBuilder: (context, index) => SizedBox(
-      height: AppSpacing.s16,
-    ),
+    separatorBuilder: (context, index) => AppGap.v16,
     itemCount: subAlerts.length,
   );
 
@@ -121,143 +144,5 @@ class _AlertScreenState extends State<AlertScreen> {
         ),
       );
     },
-  );
-}
-
-class _AlertItem extends StatelessWidget {
-  final String alertType;
-  final Duration sendAt;
-  final String? comment;
-  final String profileImage;
-
-  const _AlertItem({
-    required this.profileImage,
-    required this.alertType,
-    required this.sendAt,
-    this.comment,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        _profile(),
-        AppGap.h12,
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _title(),
-              if (comment != null)
-                Text(
-                  comment!,
-                  style: AppTextStyles.caption(textColor: AppColors.gray60),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-
-  final double _profileSize = 24;
-
-  Widget _profile() => Container(
-    height: _profileSize,
-    width: _profileSize,
-    decoration: BoxDecoration(
-      borderRadius: AppRadius.circleRadius,
-    ),
-    child: SvgPicture.asset(profileImage),
-  );
-
-  Widget _title() => Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-      Text(
-        alertType,
-        style: AppTextStyles.caption(textColor: AppColors.gray90),
-      ),
-      Text(
-        "${sendAt.inHours}시간 전",
-        style: AppTextStyles.caption(textColor: AppColors.gray60),
-      ),
-    ],
-  );
-}
-
-class _ReportAlertItem extends StatelessWidget {
-  final String profileImage;
-  final String reason;
-  final DateTime restrictAt;
-  final Duration restrictDuration;
-
-  const _ReportAlertItem({
-    required this.profileImage,
-    required this.reason,
-    required this.restrictAt,
-    required this.restrictDuration,
-  });
-
-  String _dateFormat(DateTime date) => "${date.year}-${date.month}-${date.day}";
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: AppPadding.alertReportItem,
-      decoration: BoxDecoration(
-        borderRadius: AppRadius.baseRadius,
-        color: AppColors.redLight,
-      ),
-      child: Row(
-        children: [
-          _profile(),
-
-          AppGap.h12,
-
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "신고가 누적되어 커피챗 및 커뮤티니 활동이 제한되었습니다.",
-                  style: AppTextStyles.caption(textColor: AppColors.gray90),
-                ),
-                _content(),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  final double _profileSize = 24;
-
-  Widget _profile() => Container(
-    height: _profileSize,
-    width: _profileSize,
-    decoration: BoxDecoration(
-      borderRadius: AppRadius.circleRadius,
-    ),
-    child: SvgPicture.asset(profileImage),
-  );
-
-  Widget _content() => Column(
-    mainAxisSize: MainAxisSize.min,
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(
-        "시유: $reason",
-        style: AppTextStyles.caption(textColor: AppColors.gray70),
-      ),
-      Text(
-        "기간: 제한 된 날로부터 ${restrictDuration.inDays}일간: ${_dateFormat(restrictAt)} - ${_dateFormat(restrictAt.add(restrictDuration))}",
-        style: AppTextStyles.caption(textColor: AppColors.gray70),
-      ),
-    ],
   );
 }

10958b19b8400ade2b73df22e56f0244f33b92b6
master <s25013@gsm.hs.kr>
2026-02-21 15:20:41 +0900

    feature: 채팅 입력 텍스트 필드 위젯 정의
    - chat_input_field.dart 파일
        - send 함수만 정의
    
    Resolve: #69

---
+++ lib/presentation/chat/widgets/chat_input_field.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:ondo/core/design_system/app_colors.dart';
+import 'package:ondo/core/design_system/app_icon.dart';
+import 'package:ondo/core/design_system/app_layout.dart';
+import 'package:ondo/core/design_system/components/custom_textfield.dart';
+import 'package:ondo/presentation/chat/controllers/chat_room_controller.dart';
+
+class ChatInputField extends StatelessWidget {
+  ChatInputField({super.key});
+
+  //final ChatRoomController _controller = Get.find<ChatRoomController>();
+
+  final TextEditingController _textEditingController = TextEditingController();
+
+
+  void _send () {}
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: AppPadding.textField,
+      color: AppColors.white,
+      child: Row(
+        children: [
+          Expanded(
+            child: CustomTextField(
+              controller: _textEditingController,
+              hintText: "메세지 내용을 입력해 주세요",
+              maxLines: 2,
+            ),
+          ),
+          AppGap.h16,
+          _sendButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _sendButton() => GestureDetector(
+    onTap: _send,
+    child: SvgPicture.asset(
+      AppIcon.send.path,
+      width: 36,
+      height: 18,
+    ),
+  );
+}

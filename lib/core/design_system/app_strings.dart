9bbef2cd4b663e79539fa36a6ac5553d258d063f
suehng0823 <s25010@gsm.hs.kr>
2026-02-22 15:44:22 +0900

    design : 화면에 사용될 텍스트 추가

--- lib/core/design_system/app_strings.dart
+++ lib/core/design_system/app_strings.dart
@@ -63,21 +63,17 @@ class AppStrings {
   static const ruleViolationWarning =
       '위 규칙을 상습적으로 지키지 않을 시에 관리자에게 \n제재를 받을 수 있어요!';
 
-  static const emailCodeInputHint =
-      '인증번호 6자리를 입력해주세요';
+  static const emailCodeInputHint = '인증번호 6자리를 입력해주세요';
 
-  static const invalidEmailCode =
-      '인증번호가 올바르지 않아요. 다시 확인해주세요';
+  static const invalidEmailCode = '인증번호가 올바르지 않아요. 다시 확인해주세요';
 
-  static const inputEmailAndPassword =
-  '이메일과 비밀번호를 입력해주세요.';
+  static const inputEmailAndPassword = '이메일과 비밀번호를 입력해주세요.';
 
-  static const emailRegex =
-  '이메일 형식이 올바르지 않아요.';
+  static const emailRegex = '이메일 형식이 올바르지 않아요.';
 
-  static const passwordLength =
-  '비밀번호는 8~15자여야 해요.';
+  static const passwordLength = '비밀번호는 8~15자여야 해요.';
 
-  static const passwordRegex =
-  '비밀번호에 특수기호를 포함해주세요.';
+  static const passwordRegex = '비밀번호에 특수기호를 포함해주세요.';
+
+  static const PasswardResetEmail = '계정에 등록된\n이메일을 입력해주세요';
 }

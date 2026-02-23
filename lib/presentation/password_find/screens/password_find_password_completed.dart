054c735378b1eb90f65d5eba976ed91f8b71a34c
suehng0823 <s25010@gsm.hs.kr>
2026-02-23 14:28:20 +0900

    feature : 비밀번호 재설정 화면 퍼블리싱

---
+++ lib/presentation/password_find/screens/password_find_password_completed.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:ondo/core/design_system/app_colors.dart';
+import 'package:ondo/core/design_system/app_layout.dart';
+import 'package:ondo/core/design_system/app_strings.dart';
+import 'package:ondo/core/design_system/app_text_styles.dart';
+import 'package:ondo/core/design_system/component_variants.dart';
+import 'package:ondo/core/design_system/components/custom_button.dart';
+import 'package:ondo/core/ui/base/base_scaffold.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: PasswordSetupScreen(email: 'bulgom.com'),
+    ),
+  );
+}
+
+class PasswordSetupScreen extends StatelessWidget {
+  final String email;
+
+  const PasswordSetupScreen({super.key, required this.email});
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseScaffold(
+      body: Padding(
+        padding: AppPadding.screenHorizontal,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppGap.v51,
+            Text(
+              '$email로 회원가입된 계정의',
+              style: AppTextStyles.caption(textColor: AppColors.gray70),
+            ),
+            AppGap.v8,
+            Text(
+              AppStrings.passwordResetCompleted,
+              style: AppTextStyles.titleSm(),
+            ),
+            Spacer(flex: 1),
+            Column(
+              children: [
+                CustomButton(
+                  text: '다음',
+                  variant: ButtonVariant.primary,
+                  onPressed: () {},
+                ),
+                AppGap.v16,
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

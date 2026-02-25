757fa764b4462a2cc24dbcc64cd456eb00e892e6
master <s25013@gsm.hs.kr>
2026-02-25 15:22:11 +0900

    feature: 커뮤니티 필터 테그 리스트 정의
    - community_filter_tag_list.dart 파일 생성
        - 커뮤티니 페이지 내 필터 적용 테그들
    
    Fix: #83

---
+++ lib/presentation/community/widgets/community_filter_tag_list.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ondo/core/design_system/app_layout.dart';
+import 'package:ondo/core/design_system/components/custom_tag_card.dart';
+import 'package:ondo/presentation/community/controllers/community_filter_controller.dart';
+
+class CommunityFilterTagList extends GetView<CommunityFilterController> {
+  const CommunityFilterTagList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: AppSpacing.s36,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemCount: controller.filterTags.length,
+        separatorBuilder: (context, index) => AppGap.h16,
+        itemBuilder: (context, index) => CustomTagCard(
+          tag: controller.filterTags[index],
+          onTap: controller.filterPosts,
+        ),
+      ),
+    );
+  }
+}

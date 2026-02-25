d288a157a9d44a21884bdab6035765958007db6f
user <s25013@gsm.hs.kr>
2026-02-26 03:16:50 +0900

    feature : 커뮤니티 게시물 상세 페이지 전체 구조 구현
    - community_post_detail_screen.dart 파일
        - PageViewController 종속성 활용
        - 기존의 Widget들 GetView로 전환
        - CommentList, RelatedPostList 추가 위젯 정의
    
    - nickname_setup_screen.dart 파일 전역 controller 내부로 이동
    
    Fix : #88

--- lib/presentation/community/screens/community_post_detail_screen.dart
+++ lib/presentation/community/screens/community_post_detail_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ondo/core/design_system/app_colors.dart';
@@ -6,9 +8,13 @@ import 'package:ondo/core/design_system/app_layout.dart';
 import 'package:ondo/core/design_system/app_text_styles.dart';
 import 'package:ondo/core/design_system/components/custom_back_button.dart';
 import 'package:ondo/core/design_system/components/custom_profile_circle.dart';
+import 'package:ondo/core/design_system/components/post/indicator_post_page_list.dart';
+import 'package:ondo/core/design_system/components/post/post_item.dart';
+import 'package:ondo/core/design_system/components/post/post_list_indicator.dart';
 import 'package:ondo/core/ui/base/base_scaffold.dart';
 import 'package:ondo/presentation/community/controllers/post_view_controller.dart';
 import 'package:ondo/presentation/community/widgets/community_custom_icon_button.dart';
+import 'package:ondo/presentation/community/widgets/community_post_comment_card.dart';
 
 
 class CommunityPostDetailScreen extends StatefulWidget {
@@ -33,8 +39,15 @@ class _CommunityPostDetailScreenState extends State<CommunityPostDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return BaseScaffold(
-      body: Column(
-        children: [..._top(), AppGap.v16, _body()],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ..._top(),
+            AppGap.v16,
+            _body(),
+            footer(),
+          ],
+        ),
       ),
     );
   }
@@ -51,7 +64,7 @@ class _CommunityPostDetailScreenState extends State<CommunityPostDetailScreen> {
         ],
       ],
     ),
-    _Title(title: "요즘 UI UX", tags: ["#UI/UX", "#FrontEnd"]),
+    _Title(),
   ];
 
   Widget _body() => Padding(
@@ -59,6 +72,19 @@ class _CommunityPostDetailScreenState extends State<CommunityPostDetailScreen> {
     child: Column(
       children: [
         _Body(),
+        AppGap.v24,
+        _CommentList(),
+      ],
+    ),
+  );
+
+  Widget footer() => Container(
+    color: AppColors.background,
+    padding: AppPadding.screenHorizontal,
+    child: Column(
+      children: [
+        AppGap.v16,
+        _RelatedPostList(),
       ],
     ),
   );
@@ -69,58 +95,42 @@ class _CommunityPostDetailScreenState extends State<CommunityPostDetailScreen> {
   );
 }
 
-class _Title extends StatelessWidget {
-  const _Title({required this.title, required this.tags});
-
-  final String title;
-  final List<String> tags;
-
+class _Title extends GetView<PostViewController> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: AppPadding.screenHorizontal,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: AppTextStyles.titleBold20(textColor: AppColors.gray90),
-          ),
-          AppGap.v16,
-          Row(
-            spacing: AppSpacing.s16,
-            children: tags
-                .map(
-                  (tag) => Text(
-                    tag,
-                    style: AppTextStyles.caption(textColor: AppColors.gray60),
-                  ),
-                )
-                .toList(),
-          ),
-        ],
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              controller.title.value,
+              style: AppTextStyles.titleBold20(textColor: AppColors.gray90),
+              overflow: TextOverflow.ellipsis,
+            ),
+            AppGap.v16,
+            Wrap(
+              spacing: AppSpacing.s16,
+              children: controller.postTags
+                  .map(
+                    (tag) => Text(
+                      tag,
+                      style: AppTextStyles.caption(textColor: AppColors.gray60),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  )
+                  .toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-class _Body extends StatelessWidget {
-  _Body();
-
-  final String author = "김유찬";
-  final Duration postAt = Duration(minutes: 4);
-  final String bodyText = """요즘 UI·UX 이슈 보면, 기술은 엄청 발전했는데 사용자 입장은 좀 
-애매해짐. AI 추천이니 자동 생성이니 많아졌는데, 솔직히 왜 그게 뜨는지 모르겠음. 내가 선택한 건지, 그냥 떠밀린 건지 경계가 흐려짐.
-
-개인화도 마찬가지임. 처음엔 편한데 쓰다 보면 피곤함. 설정 건드릴 
-여지도 없고, 앱이 “너 이거 좋아하잖아” 하고 단정 짓는 느낌 듦. 다크 
-패턴은 말할 것도 없고, 가입은 한 번에 되는데 탈퇴는 왜 이렇게 
-숨겨놓는지 아직도 이해 안 감.
-
-접근성도 형식만 맞춘 경우 많음. 체크리스트는 통과했는데 실제로 써보면 불편함. 애니메이션, 효과 잔뜩 넣은 UI도 처음엔 와 소리 나오는데, 정보 찾으려면 오히려 방해됨.
-
-결국 요즘 UX 문제는 기술 부족이 아니라 태도 문제 같음. 사용자를 배려한다면서 사실은 컨트롤하려는 설계. 이제는 “얼마나 오래 쓰게 만드나” 말고 “얼마나 덜 스트레스 받게 하나”로 가야 하지 않나 싶음.""";
-
+class _Body extends GetView<PostViewController> {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -140,14 +150,14 @@ class _Body extends StatelessWidget {
       children: [
         CustomProfileCircle(radius: AppSpacing.s24),
         AppGap.h12,
-        Expanded(child: Text(author)),
-        Text("${postAt.inMinutes}분전"),
+        Expanded(child: Text(controller.authorName.value)),
+        Text("${controller.postAt.value.inMinutes}분전"),
       ],
     ),
   );
 
   Widget _content() => Text(
-    bodyText,
+    controller.bodyText.value,
     style: AppTextStyles.textMedium(textColor: AppColors.gray90),
     textAlign: .start,
   );
@@ -157,17 +167,142 @@ class _Body extends StatelessWidget {
     children: [
       CommunityCustomIconButton(
         imagePath: AppIcon.heart.path,
-        action: (isSelect, total) {},
+        action: (isSelect, total) {
+          controller.selectHeart = isSelect;
+          controller.heartTotal = total;
+        },
         activeColor: AppColors.red,
-        total: 12,
+        total: controller.heartTotal,
+        initialIsSelected: controller.selectHeart,
       ),
       CommunityCustomIconButton(
         imagePath: AppIcon.bookmark.path,
-        action: (isSelect, total) {},
+        action: (isSelect, total) {
+          controller.selectBookMark = isSelect;
+          controller.bookMarkTotal = total;
+        },
         activeColor: AppColors.yellow,
-        total: 12,
-        initialSelect: true,
+        total: controller.bookMarkTotal,
+        initialIsSelected: controller.selectBookMark,
       ),
     ],
   );
+}
+
+class _CommentList extends StatefulWidget {
+  @override
+  State<_CommentList> createState() => _CommentListState();
+}
+
+class _CommentListState extends State<_CommentList> {
+  late final PageController _pageController;
+  final PostViewController _controller = Get.find<PostViewController>();
+
+  final ValueNotifier<int> curIndex = ValueNotifier(0);
+
+  @override
+  void initState() {
+    _pageController = PageController();
+    super.initState();
+  }
+
+  int _getStartIndex(int index) => index * 4;
+
+  int _getLastIndex(int index) =>
+      min(_getStartIndex(index) + 4, _controller.comments.length);
+
+  int _getTotalPage() => (_controller.comments.length / 4).ceil();
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxHeight: 360,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _title(),
+          AppGap.v16,
+          Expanded(
+            child: Obx(
+              () => PageView.builder(
+                onPageChanged: (value) {
+                  curIndex.value = value;
+                },
+                itemCount: _getTotalPage(),
+                itemBuilder: (context, index) => Center(
+                  child: _commentView(
+                    _getStartIndex(index),
+                    _getLastIndex(index),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          AppGap.v16,
+          ValueListenableBuilder(
+            valueListenable: curIndex,
+            builder: (context, value, _) => PostListIndicator(
+              currentPage: value,
+              totalPage: _getTotalPage(),
+              onTap: (value) {
+                _pageController.jumpToPage(value);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _title() => Row(
+    children: [
+      Text("댓글", style: AppTextStyles.titleSm16(textColor: AppColors.gray90)),
+      AppGap.h12,
+      Text(
+        "${_controller.comments.length}",
+        style: AppTextStyles.textMedium(textColor: AppColors.gray60),
+      ),
+    ],
+  );
+
+  Widget _commentView(int startIndex, int lastIndex) {
+    final subComments = _controller.comments.sublist(startIndex, lastIndex);
+    return Column(
+      spacing: AppSpacing.s16,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        ...subComments.map(
+          (comment) => CommunityPostCommentCard(
+            author: comment.author,
+            commentText: comment.conment,
+            heartTotal: comment.heartTotal,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _RelatedPostList extends StatelessWidget {
+  _RelatedPostList();
+
+  final List<PostItem> posts = List.generate(
+    32,
+    (index) => PostItem(
+      skills: ["UI/UX", "FrontEnd"],
+      title: "요즘 UI UX",
+      author: "김유찬",
+      bookmarks: 12,
+      favorites: 12,
+      createMinutes: 4,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return IndicatorPostPageList(title: "관련 게시물", items: posts);
+  }
 }

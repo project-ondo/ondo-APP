027f2b49a90e9e736893e2a92ff7b8af07fb543d
suehng0823 <s25010@gsm.hs.kr>
2026-02-26 22:39:04 +0900

    refactor : get 컨트롤러 put -> find로 수정

--- lib/presentation/community/widgets/tag_input_field.dart
+++ lib/presentation/community/widgets/tag_input_field.dart
@@ -11,7 +11,7 @@ class TagInputField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = Get.put(TagInputController());
+    final controller = Get.find<TagInputController>();
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,

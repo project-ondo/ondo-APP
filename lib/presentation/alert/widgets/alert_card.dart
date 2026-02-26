9509584d326757c65c5ffebfac9967dd2cf6bb00
master <s25013@gsm.hs.kr>
2026-02-26 10:30:08 +0900

    feature: 알림 카드 위젯 리펙토링
    - alert_card.dart 파일
        - component 이용 (프로필)
        - 텍스트 overflow 처리
        - onTap (엑션 정의)
    
    Fix: #86

--- lib/presentation/alert/widgets/alert_card.dart
+++ lib/presentation/alert/widgets/alert_card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:ondo/core/design_system/components/custom_profile_circle.dart';
 
 import '../../../core/design_system/app_colors.dart';
 import '../../../core/design_system/app_layout.dart';
@@ -9,59 +9,58 @@ class AlertCard extends StatelessWidget {
   final String alertType;
   final Duration sendAt;
   final String? comment;
-  final String profileImage;
+  final String? profileImg;
+  final VoidCallback? onTap;
 
   const AlertCard({
     super.key,
-    required this.profileImage,
+    this.profileImg,
     required this.alertType,
     required this.sendAt,
     this.comment,
+    this.onTap
   });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        _profile(),
-        AppGap.h12,
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _title(),
-              if (comment != null)
-                Text(
-                  comment!,
-                  style: AppTextStyles.caption(textColor: AppColors.gray60),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-            ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Row(
+        children: [
+          CustomProfileCircle(
+            radius: AppSpacing.s24,
+            imageUrl: profileImg,
           ),
-        ),
-      ],
+          AppGap.h12,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _title(),
+                if (comment != null)
+                  Text(
+                    comment!,
+                    style: AppTextStyles.caption(textColor: AppColors.gray60),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
-  final double _profileSize = AppSpacing.s24;
-
-  Widget _profile() => Container(
-    height: _profileSize,
-    width: _profileSize,
-    decoration: BoxDecoration(
-      borderRadius: AppRadius.circleRadius,
-    ),
-    child: SvgPicture.asset(profileImage),
-  );
-
   Widget _title() => Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
     children: [
       Text(
         alertType,
         style: AppTextStyles.caption(textColor: AppColors.gray90),
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
       ),
+      Spacer(),
       Text(
         "${sendAt.inHours}시간 전",
         style: AppTextStyles.caption(textColor: AppColors.gray60),

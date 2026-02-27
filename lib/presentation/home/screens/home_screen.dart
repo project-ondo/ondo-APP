658656b23453d9ef9af2fb29d70db615514c8635
master <s25013@gsm.hs.kr>
2026-02-27 15:51:57 +0900

    refactor: 홈 검색 결과 페이지 controller 종속성 연결
    - home_screen.dart 파일
        - HomeSearchResultController 등록
    
    - home_search_page.dart 파일
        - Base 위젯 이용
        - controller 연결
    
    Fix: #92

--- lib/presentation/home/screens/home_screen.dart
+++ lib/presentation/home/screens/home_screen.dart
@@ -4,12 +4,12 @@ import 'package:ondo/core/design_system/app_colors.dart';
 import 'package:ondo/core/design_system/app_layout.dart';
 import 'package:ondo/core/ui/base/base_scaffold.dart';
 import 'package:ondo/presentation/home/controllers/home_controller.dart';
-import 'package:ondo/presentation/home/screens/home_search_detail_screen.dart';
+import 'package:ondo/presentation/home/screens/home_search_page.dart';
 import 'package:ondo/presentation/home/widgets/home_post_rank_list.dart';
 import 'package:ondo/presentation/home/widgets/home_recommend_chat_list.dart';
 import 'package:ondo/presentation/home/widgets/home_recommend_post_list.dart';
 
-import '../../../core/design_system/components/top_bar/main_top_search_bar.dart';
+import '../../search/widgets/main_top_search_bar.dart';
 
 
 
@@ -32,8 +32,8 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
         resultPageBuilder: (resultModel) {
-          final data = resultModel as HomeSearchModel;
-          return HomeSearchDetailPage(chats: data.chats, posts: data.posts);
+          Get.put(HomeSearchResultController());
+          return HomeSearchDetailPage();
         },
       ),
     );

c6d9e68e649a2e61e27cf509b53bcfc5c07d1430
master <s25013@gsm.hs.kr>
2026-02-27 14:16:48 +0900

    feature: 홈 화면 controller 정의
    - home_controller.dart 파일 생성
        - ranks, chat, posts 정의
        - 데이터 모델 정의
        - 임시 데이터 정의
        - 랭크 정렬 기능 추가
    
    Fix: #92

---
+++ lib/presentation/home/controllers/home_controller.dart
@@ -0,0 +1,87 @@
+import 'dart:math';
+
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  final RxList<HomeRecentPopularPostInfo> ranks =
+      <HomeRecentPopularPostInfo>[].obs;
+  final RxList<HomeRecommendProfileInfo> chats =
+      <HomeRecommendProfileInfo>[].obs;
+  final RxList<HomeRecommendPostInfo> posts = <HomeRecommendPostInfo>[].obs;
+
+  @override
+  void onInit() {
+    _sortRating(ranks..addAll(getRanks()));
+    chats.addAll(getChats());
+    posts.addAll(getPosts());
+    super.onInit();
+  }
+
+  void _sortRating(RxList<HomeRecentPopularPostInfo>? list) => list == null
+      ? ranks.sort((a, b) => (b.favorites - a.favorites))
+      : list.sort((a, b) => b.favorites - a.favorites);
+}
+
+extension DummyData on HomeController {
+  List<HomeRecentPopularPostInfo> getRanks() => [
+    for (int i = 1; i < 5; i++) ...{
+      (
+        title: "요즘 공부 어케 하시나요 다들",
+        creatAt: Duration(days: 3),
+        favorites: 160 * Random().nextInt(i),
+        isFavorite: i % 2 == 0,
+      ),
+      (
+        title: "10년차 개발자는 무슨 공부할까",
+        creatAt: Duration(days: 5),
+        favorites: 121 * Random().nextInt(i),
+        isFavorite: i % 2 == 0,
+      ),
+      (
+        title: "팀장 퇴사해서 디자인빵꾸남",
+        creatAt: Duration(days: 2),
+        favorites: 73 * Random().nextInt(i),
+        isFavorite: i % 2 == 0,
+      ),
+    },
+  ];
+
+  List<HomeRecommendProfileInfo> getChats() => [
+    for (int i = 1; i < 6; i++) ...{
+      (name: "김유찬", skill: " UI/UX", rating: i),
+    },
+  ];
+
+  List<HomeRecommendPostInfo> getPosts() => [
+    for (int i = 0; i < 4; i++) ...{
+      (
+        name: "김유찬",
+        title: "요즘 UI UX",
+        skills: ["UI/UX", "FrontEnd"],
+        bookmarks: 12,
+        favoites: 12,
+        createAt: Duration(minutes: i),
+        isFavorite: i % 2 == 0,
+        isBookmark: i % 3 == 0,
+      ),
+    },
+  ];
+}
+
+typedef HomeRecentPopularPostInfo = ({
+  String title,
+  Duration creatAt,
+  int favorites,
+  bool isFavorite,
+});
+typedef HomeRecommendProfileInfo = ({String name, String skill, int rating});
+typedef HomeRecommendPostInfo = ({
+  List<String> skills,
+  String title,
+  String name,
+  int favoites,
+  int bookmarks,
+  Duration createAt,
+  bool isBookmark,
+  bool isFavorite,
+});

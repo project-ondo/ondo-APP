2d2add3814204ed96083d768be0c6b184d6726a5
heoeunseo08 <s24053@gsm.hs.kr>
2026-03-03 23:31:37 +0900

    feature : "비밀번호 재설정 완료 단계 구현"
    - PasswordResetCompletedController 생성
    - 완료 화면 구성
    - flow clear 처리 추가
    
    Resolve: #94

--- lib/presentation/auth/password_reset/screens/password_reset_password_completed.dart
+++ lib/presentation/auth/password_reset/screens/password_reset_password_completed.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 import 'package:ondo/core/design_system/app_colors.dart';
 import 'package:ondo/core/design_system/app_layout.dart';
 import 'package:ondo/core/design_system/app_strings.dart';
 import 'package:ondo/core/design_system/app_text_styles.dart';
 import 'package:ondo/core/design_system/component_variants.dart';
 import 'package:ondo/core/design_system/components/custom_button.dart';
+import 'package:ondo/core/router/app_router.dart';
 import 'package:ondo/core/ui/base/base_scaffold.dart';
+import 'package:ondo/presentation/auth/password_reset/controllers/password_reset_completed_controller.dart';
 
-
-class PasswordResetCompletedScreen extends StatelessWidget {
-  final String email;
-
-  const PasswordResetCompletedScreen({super.key, required this.email});
+class PasswordResetCompletedScreen
+    extends GetView<PasswordResetCompletedController> {
+  const PasswordResetCompletedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -23,21 +25,23 @@ class PasswordResetCompletedScreen extends StatelessWidget {
           children: [
             AppGap.v51,
             Text(
-              '$email로 회원가입된 계정의',
+              '${controller.email}(으)로 회원가입된 계정의',
               style: AppTextStyles.caption(textColor: AppColors.gray70),
             ),
             AppGap.v8,
             Text(
               AppStrings.passwordResetCompleted,
-              style: AppTextStyles.titleSm(),
+              style: AppTextStyles.titleSm(textColor: AppColors.gray90),
             ),
             Spacer(flex: 1),
             Column(
               children: [
                 CustomButton(
-                  text: '다음',
+                  text: '로그인으로',
                   variant: ButtonVariant.primary,
-                  onPressed: () {},
+                  onPressed: () {
+                    context.go(RoutePaths.login);
+                  },
                 ),
                 AppGap.v16,
               ],

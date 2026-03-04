70339e2131bf8a73d7c3f817d6d10f090f24aad2
heoeunseo <heoeunseong140@gmail.com>
2026-03-05 01:01:34 +0900

    refactor : "불필요한 import제거"
    login_controller.dart: import 'package:go_router/go_router.dart';, import 'package:ondo/core/router/app_router.dart'; 삭제
    
    Fix: #94

--- lib/presentation/auth/login/controllers/login_controller.dart
+++ lib/presentation/auth/login/controllers/login_controller.dart
@@ -1,8 +1,6 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:ondo/core/design_system/app_strings.dart';
-import 'package:ondo/core/router/app_router.dart';
 
 class LoginController extends GetxController {
   final emailController = TextEditingController();

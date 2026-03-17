14de980ed2a1b3fdb78591b7fd9d5d2ba28fe9cf
user <s25013@gsm.hs.kr>
2026-03-18 00:18:51 +0900

    refactor : community 컨트롤러 구조 개선 및 TODO 주석 추가
    - community_controller.dart 파일
        - search 함수 추가 (cache로 data 조회)
        - filter 함수 추가 (cache로 data 조회)
    
    - community_main_screen.dart 파일
        - 검색 구조 반영
    
    fix: #92

--- lib/presentation/community/controllers/community_controller.dart
+++ lib/presentation/community/controllers/community_controller.dart
@@ -1,32 +1,89 @@
 import 'package:get/get.dart';
+import 'package:ondo/presentation/community/controllers/community_post_create_screen_controller.dart';
+import 'package:ondo/presentation/community/screens/community_post_create_screen.dart';
 import 'package:ondo/presentation/home/controllers/home_controller.dart';
 
 class CommunityController extends GetxController {
-  RxList<String> filterTags = <String>[].obs;
-  late final CommunitySearchResultController searchResultController;
+  final List<String> tags = <String>[].obs;
+  final RxSet<String> selectTagList = <String>{}.obs;
+  final List<PostInfo> _cachePosts = <PostInfo>[];
+  final RxList<PostInfo> viewPosts = <PostInfo>[].obs;
 
   @override
   void onInit() {
-    filterTags.addAll(_getTags());
+    tags.addAll(_getTags());
+    _cachePosts.addAll(_getPosts());
+    viewPosts.addAll(_cachePosts);
     super.onInit();
   }
 
   @override
   void onReady() {
-    searchResultController = Get.put(CommunitySearchResultController());
+    Get.put(CommunityResultController());
     super.onReady();
   }
 
-  void filterPosts(String filterString) {}
+  void enterPostCreate() {
+    Get.lazyPut(() => CommunityPostCreateController());
+    Get.to(CommunityPostCreateScreen());
+  }
+
+  void searchPost(List<String> searchList) {
+    //TODO: 커뮤니티 서버 검색 결과 반환
+    final Set<PostInfo> result = {};
+    result.addAllIf(
+      searchList.isNotEmpty,
+      _cachePosts.where(
+        (post) =>
+            searchList.any(
+              (search) => post.title.contains(search),
+            ) ||
+            searchList.any(
+              (search) => post.name.contains(search),
+            ) ||
+            searchList.any(
+              (search) => post.skills.any(
+                (skill) => skill.contains(search),
+              ),
+            ),
+      ),
+    );
+    Get.find<CommunityResultController>().updateResult(result);
+  }
+
+  void filterPostTag(String tag, bool isSelect) {
+    //TODO: 커뮤니티 서버 필터 검색 결과 반환
+    isSelect ? selectTagList.add(tag) : selectTagList.remove(tag);
+    if (selectTagList.isEmpty) {
+      viewPosts.assignAll(_cachePosts);
+      return;
+    }
+    final Set<PostInfo> result = {};
+    result.addAll(
+      _cachePosts.where(
+        (post) =>
+            selectTagList.any(
+              (tag) => post.title.contains(tag),
+            ) ||
+            selectTagList.any(
+              (tag) => post.name.contains(tag),
+            ) ||
+            selectTagList.any(
+              (tag) => post.skills.any(
+                (skill) => skill.contains(tag),
+              ),
+            ),
+      ),
+    );
+    viewPosts.assignAll(result);
+  }
 }
 
-class CommunitySearchResultController extends GetxController {
-  List<PostInfo> posts = <PostInfo>[].obs;
+class CommunityResultController extends GetxController {
+  final RxList<PostInfo> viewPosts = <PostInfo>[].obs;
 
-  void searchResultInfo(String text) {
-    //TODO: 커뮤니티 서버 검색 결과 반환
-    if(text.isEmpty) posts.clear();
-    posts.assignAll(_getPosts());
+  void updateResult(Iterable<PostInfo> results) {
+    viewPosts.assignAll(results);
   }
 }
 
@@ -46,7 +103,7 @@ List<PostInfo> _getPosts() => [
       skills: ["UI/UX", "FrontEnd"],
       bookmarks: 12,
       favoites: 12,
-      createAt: Duration(minutes: i),
+      createAt: Duration(minutes: i + 1),
       isFavorite: i % 2 == 0,
       isBookmark: i % 3 == 0,
     ),
--- lib/presentation/community/screens/community_main_screen.dart
+++ lib/presentation/community/screens/community_main_screen.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ondo/core/design_system/app_colors.dart';
 import 'package:ondo/core/design_system/app_layout.dart';
+import 'package:ondo/presentation/search/states/search_state.dart';
 import 'package:ondo/presentation/search/widgets/main_top_search_bar.dart';
 import 'package:ondo/core/ui/base/base_scaffold.dart';
 import 'package:ondo/presentation/community/controllers/community_controller.dart';
-import 'package:ondo/presentation/community/screens/community_search_page.dart';
+import 'package:ondo/presentation/community/screens/community_search_screen.dart';
 import 'package:ondo/presentation/community/widgets/community_filter_tag_list.dart';
 import 'package:ondo/presentation/community/widgets/community_post_add_button.dart';
 import 'package:ondo/presentation/community/widgets/community_post_list.dart';
@@ -36,9 +37,9 @@ class CommunityMainScreen extends GetView<CommunityController> {
           ),
         ),
         resultPageBuilder: (state) {
-          if (state.query.trim().isEmpty) return null;
-          controller.searchResultController.searchResultInfo(state.query.value);
-          return CommunitySearchPage();
+          if (state.query.trim().isEmpty && state.tags.isEmpty) return null;
+          controller.searchPost([state.query, ...state.tags]);
+          return CommunitySearchScreen();
         },
       ),
     );

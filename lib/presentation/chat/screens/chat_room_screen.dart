769ecb47eebdfbe8eaeb50f5b59198069ba5fced
user <s25013@gsm.hs.kr>
2026-03-18 00:13:36 +0900

    refactor : chat room 컨트롤러 구조 개선 및 TODO 주석 추가
    - chat_controller.dart 파일
        - dialog 표시 함수 controller에서 관리
        - send 함수 정의
    
    - chat_room_screen.dart 파일
        - TODO 추가 및 일부 변경된 사항에 맞는 구조 개선
        - GetView 적용
    
    fix: #92

--- lib/presentation/chat/screens/chat_room_screen.dart
+++ lib/presentation/chat/screens/chat_room_screen.dart
@@ -5,52 +5,14 @@ import 'package:ondo/core/design_system/app_colors.dart';
 import 'package:ondo/core/design_system/app_icon.dart';
 import 'package:ondo/core/design_system/app_layout.dart';
 import 'package:ondo/core/design_system/app_text_styles.dart';
-import 'package:ondo/core/design_system/components/custom_alert_dialog.dart';
 import 'package:ondo/core/design_system/components/custom_back_button.dart';
 import 'package:ondo/core/ui/base/base_scaffold.dart';
 import 'package:ondo/presentation/chat/controllers/chat_controller.dart';
 import 'package:ondo/presentation/chat/widgets/chat_card.dart';
 import 'package:ondo/presentation/chat/widgets/chat_input_field.dart';
-import 'package:ondo/presentation/chat/widgets/chat_review_dialog.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: ChatRoomScreen(),
-    ),
-  );
-}
-
-class ChatRoomScreen extends StatefulWidget {
+class ChatRoomScreen extends GetView<ChatController> {
   const ChatRoomScreen({super.key});
-
-  @override
-  State<ChatRoomScreen> createState() => _ChatRoomScreenState();
-}
-
-class _ChatRoomScreenState extends State<ChatRoomScreen> {
-  final ChatController _controller = Get.put(ChatController());
-
-  void _showReviewDialog() => showDialog(
-    context: context,
-    builder: (context) => ChatReviewDialog(),
-  );
-
-  void _showQuitAlertDialog() => showDialog(
-    context: context,
-    builder: (context) => CustomAlertDialog(
-      title: "커피챗 종료",
-      comment: "정말 커피챗을 종료하시겠어요?",
-      actionLeft: () => Navigator.pop(context),
-      actionRight: () {
-        Navigator.pop(context);
-        _showReviewDialog();
-      },
-      rightActionText: "다음",
-    ),
-  );
-
-  void _report() {}
 
   @override
   Widget build(BuildContext context) {
@@ -69,19 +31,16 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
     width: double.maxFinite,
     color: AppColors.background,
     child: Obx(
-      () => !_controller.isNewChat.value ? _chatList() : _noChatIcon(),
+      () => controller.chats.isNotEmpty ? _chatList() : _noChatIcon(),
     ),
   );
 
   Widget _chatList() => ListView.builder(
     itemBuilder: (context, index) {
-      final chat = _controller.chats[index];
-      if (chat.isMe) {
-        return _myChat(chat.comment);
-      }
-      return _otherChat(chat.comment);
+      final chat = controller.chats[index];
+      return chat.isMe ? _myChat(chat.comment) : _otherChat(chat.comment);
     },
-    itemCount: _controller.chats.length,
+    itemCount: controller.chats.length,
   );
 
   Widget _myChat(String text) => Row(
@@ -94,6 +53,7 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
   Widget _otherChat(String text) => Row(
     mainAxisAlignment: MainAxisAlignment.start,
     children: [
+      //TODO : chat model 정의 후, 데이터 변경
       ChatCard(
         isMe: false,
         text: text,
@@ -121,6 +81,7 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
     ],
   );
 
+  //TODO : chat model 정의 후, 데이터 변경
   Widget _topBar() => CustomBackButton(
     moreOptions: true,
     useUserProfile: true,
@@ -133,11 +94,15 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
     itemBuilder: (context) => [
       _customPopupMenu(
         "커피챗 종료하기",
-        _showQuitAlertDialog,
+        //종료 알림창 표시
+        controller.showQuitAlert,
       ),
       _customPopupMenu(
         "신고하기",
-        _report,
+        //신고 기능
+        () {
+          //TODO : 신고 기능
+        },
       ),
     ],
   );

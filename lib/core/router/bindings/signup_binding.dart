03082bae98831b3b63a96c0d54f9ef108707b262
heoeunseo <heoeunseong140@gmail.com>
2026-03-21 01:13:47 +0900

    fix : 2차 ci 오류 해결
    사용되지 않은 import파일 제거
    
    Fix: #98

--- lib/core/router/bindings/signup_binding.dart
+++ lib/core/router/bindings/signup_binding.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/bindings_interface.dart';
 import 'package:get/get_instance/src/extension_instance.dart';

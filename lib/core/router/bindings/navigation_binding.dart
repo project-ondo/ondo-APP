514d7994e1bd23efe51b9fe3fce5dde43c083f87
user <s25013@gsm.hs.kr>
2026-04-15 12:46:03 +0900

    feature : 로그인/토큰 갱신 binding 연동
    네비게이션 화면 진입 시 auth 관련 의존성 주입 연동
    
    Resolve: #103

--- lib/core/router/bindings/navigation_binding.dart
+++ lib/core/router/bindings/navigation_binding.dart
@@ -1,9 +1,12 @@
 import 'package:get/get.dart';
+import 'package:ondo/core/router/bindings/chat_binding.dart';
+import 'package:ondo/core/router/bindings/community_binding.dart';
+import 'package:ondo/core/router/bindings/home_binding.dart';
 import 'package:ondo/presentation/navigation/controllers/navigation_controller.dart';
 import 'package:ondo/presentation/post/controllers/post_controller.dart';
 
 import '../../../presentation/alert/controllers/alert_controller.dart';
-import '../../../presentation/chat/controllers/chat_main_screen_controller.dart';
+import '../../../presentation/chat/controllers/chat_controller.dart';
 import '../../../presentation/community/controllers/community_controller.dart';
 import '../../../presentation/home/controllers/home_controller.dart';
 import '../../../presentation/search/controllers/main_top_bar_search_controller.dart';
@@ -12,17 +15,11 @@ class NavigationBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<NavigationController>(() => NavigationController());
-    Get.lazyPut<MainTopBarSearchController>(
-      () => MainTopBarSearchController(),
-    );
-    Get.lazyPut<AlertController>(
-      () => AlertController(),
-    );
-    Get.lazyPut(
-      () => PostController(),
-    );
-    Get.lazyPut<HomeController>(() => HomeController());
-    Get.lazyPut<ChatMainScreenController>(() => ChatMainScreenController());
-    Get.lazyPut<CommunityController>(() => CommunityController());
+    Get.lazyPut<MainTopBarSearchController>(() => MainTopBarSearchController());
+    Get.lazyPut<AlertController>(() => AlertController());
+    Get.lazyPut(() => PostController());
+    HomeBinding().dependencies();
+    CommunityBinding().dependencies();
+    ChatBinding().dependencies();
   }
 }

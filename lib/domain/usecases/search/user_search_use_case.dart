2cbbca67ae89b41557725cbce01a7cf47b9db089
user <s25013@gsm.hs.kr>
2026-04-16 17:40:41 +0900

    feature : "UserSearchUseCase 구현"
    - UserRepository를 통해 유저 검색 후 UserEntity 리스트 반환
    
    Resolve: #106

---
+++ lib/domain/usecases/search/user_search_use_case.dart
@@ -0,0 +1,27 @@
+import 'package:ondo/domain/entities/home/recommend_user_entity.dart';
+import 'package:ondo/domain/repositories/search/user_repository.dart';
+
+class UserSearchUseCase {
+  final UserRepository repository;
+
+  UserSearchUseCase({required this.repository});
+
+  Future<List<UserEntity>> call({
+    String? keyword,
+    String? major,
+    List<String>? interests,
+    String? sort,
+    int? page,
+    int? size,
+  }) async {
+    final res = await repository.search(
+      size: size,
+      page: page,
+      interests: interests,
+      keyword: keyword,
+      major: major,
+      sort: sort,
+    );
+    return res.map((e) => UserEntity.fromUserModel(e)).toList();
+  }
+}

845ddd7610fd3f2389b81494291a9486fc523f0c
user <s25013@gsm.hs.kr>
2026-04-16 17:40:33 +0900

    feature : "UserRepository 및 UserRepositoryImpl 구현"
    - UserRepository abstract class 정의
    - UserRepositoryImpl 유저 검색 API 연동 구현
    - 응답 null 처리 추가
    
    Resolve: #106

---
+++ lib/data/repositories/search/user_repository_impl.dart
@@ -0,0 +1,54 @@
+import 'package:ondo/data/datasource/user/user_remote_datasource.dart';
+import 'package:ondo/data/models/base/request/base_search_request_model.dart';
+import 'package:ondo/data/models/user/response/user_model.dart';
+import 'package:ondo/domain/repositories/search/user_repository.dart';
+
+import '../../models/base/request/base_list_request_model.dart';
+
+class UserRepositoryImpl extends UserRepository {
+  UserRemoteDatasource remoteDatasource;
+
+  UserRepositoryImpl({required this.remoteDatasource});
+
+  @override
+  Future<List<UserModel>> search({
+    String? keyword,
+    String? major,
+    List<String>? interests,
+    String? sort,
+    int? page,
+    int? size,
+  }) async {
+    final model = BaseSearchRequestModel(
+      keyword: keyword,
+      major: major,
+      interests: interests,
+      sort: sort,
+      page: page,
+      size: size,
+    );
+
+    final json = await remoteDatasource.search(model);
+
+    if (json == null) return [];
+
+    final res = UserDataModel.fromJson(json);
+
+    return res.content;
+  }
+
+  @override
+  Future<List<UserModel>> getRecommendUserList({
+    required int page,
+    required int size,
+  }) async {
+    final model = BaseListRequestModel(size: size, page: page);
+
+    final json = await remoteDatasource.loadRecommendProfileList(model);
+    if (json != null) {
+      final res = UserDataModel.fromJson(json);
+      return res.content;
+    }
+    return [];
+  }
+}

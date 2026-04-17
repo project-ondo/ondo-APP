c38f4b72c9234d80966fb77ac5087d21564b827e
eum108 <s25013@gsm.hs.kr>
2026-04-18 08:32:14 +0900

    feature : "미읽음 알림 개수 조회 - NotificationBinding 업데이트"
    LoadUnreadNotificationCountUseCase 등록 및 NotificationController에 주입
    
    Resolve: #115

--- lib/core/router/bindings/notification_binding.dart
+++ lib/core/router/bindings/notification_binding.dart
@@ -3,6 +3,7 @@ import 'package:ondo/data/datasource/notification/notification_remote_datasource
 import 'package:ondo/data/network/clients/auth_client.dart';
 import 'package:ondo/data/repositories/notification/notification_repository_impl.dart';
 import 'package:ondo/domain/usecases/notification/load_my_notification_list_use_case.dart';
+import 'package:ondo/domain/usecases/notification/load_unread_notification_count_use_case.dart';
 
 import '../../../presentation/notification/controllers/notification_controller.dart';
 
@@ -17,18 +18,25 @@ class NotificationBinding extends Bindings {
     final loadMyNotificationListUseCase = LoadMyNotificationListUseCase(
       repository: repository,
     );
+    final loadUnreadNotificationCountUseCase =
+        LoadUnreadNotificationCountUseCase(repository: repository);
 
     ///binding 등록
     Get.lazyPut<NotificationRemoteDatasource>(() => datasource);
     Get.lazyPut<NotificationRepositoryImpl>(() => repository);
+
     Get.lazyPut<LoadMyNotificationListUseCase>(
       () => loadMyNotificationListUseCase,
+    );
+    Get.lazyPut<LoadUnreadNotificationCountUseCase>(
+      () => loadUnreadNotificationCountUseCase,
     );
 
     ///notification controller 등록
     Get.lazyPut<NotificationController>(
       () => NotificationController(
         loadMyNotificationListUseCase: loadMyNotificationListUseCase,
+        loadUnreadNotificationCountUseCase: loadUnreadNotificationCountUseCase,
       ),
     );
   }

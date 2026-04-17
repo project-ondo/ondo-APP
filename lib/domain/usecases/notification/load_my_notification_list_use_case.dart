b89be7a7005044f211089ea7c497018688437aad
eum108 <s25013@gsm.hs.kr>
2026-04-17 19:21:51 +0900

    feature : "알림 목록 조회 - LoadMyNotificationListUseCase 구현"
    알림 목록 조회 UseCase 구현
    
    Resolve: #113

---
+++ lib/domain/usecases/notification/load_my_notification_list_use_case.dart
@@ -0,0 +1,19 @@
+import 'package:ondo/domain/notification/notification_entity.dart';
+import 'package:ondo/domain/repositories/notification/notification_repository.dart';
+
+class LoadMyNotificationListUseCase {
+  final NotificationRepository repository;
+
+  LoadMyNotificationListUseCase({required this.repository});
+
+  Future<List<NotificationEntity>> call({
+    required int size,
+    required int page,
+  }) async {
+    return (await repository.loadMyNotificationModel(size, page))
+        .map(
+          (e) => NotificationEntity.fromNotificationModel(e),
+        )
+        .toList();
+  }
+}

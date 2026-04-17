91af70957b7869eaaaece96f3f7eaebf6a9ee57e
eum108 <s25013@gsm.hs.kr>
2026-04-17 19:21:44 +0900

    feature : "알림 목록 조회 - NotificationRemoteDatasource 구현"
    NotificationRemoteDatasource 구현 및
    ApiConstants에 notification 엔드포인트 추가
    
    Resolve: #113

--- lib/data/network/constants/api_constants.dart
+++ lib/data/network/constants/api_constants.dart
@@ -9,15 +9,22 @@ class ApiConstants {
 
   //.env 파일에서 설정한 환경 변수 불러오기
   static String get domain => Env.apiBaseUrl;
+
   static String get posts => "${Env.apiBaseUrl}/posts";
+
   static String get users => "${Env.apiBaseUrl}/users";
+
   static String get auth => "${Env.apiBaseUrl}/auth";
+
   static String get chats => "${Env.apiBaseUrl}/chat";
+
+  static String get notification => "${Env.apiBaseUrl}/notifications";
 
   static const baseHeader = {"Content-Type": "application/json"};
 
   //TODO : 임시 로그인 데이터
   static String get loginId => Env.loginId;
+
   static String get loginPassword => Env.loginPassword;
 
   void errorLog(Object error) => log("$logName 서버 에러 : ${error.toString()}");
@@ -26,5 +33,5 @@ class ApiConstants {
 
   void successLog(bool success) => log("$logName 서버 성공 여부 : $success");
 
-  void messageLog(String message) => log("$logName 서버 메세지 : $message");
+  void messageLog(String? message) => log("$logName 서버 메세지 : $message");
 }

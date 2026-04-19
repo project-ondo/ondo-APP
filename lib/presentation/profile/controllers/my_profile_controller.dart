5334eb990969b102f3cab15ec56b65e4090bc7c2
heoeunseo <h.eunseo6930@gmail.com>
2026-04-19 18:18:20 +0900

    feature : MyProfileController 구현
    프로필 조회, 로그아웃, 회원탈퇴 기능 구현
    
    Fix: #118

---
+++ lib/presentation/profile/controllers/my_profile_controller.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/foundation.dart';
+import 'package:get/get.dart';
+import 'package:ondo/data/datasource/auth/auth_local_datasource_impl.dart';
+import 'package:ondo/data/datasource/user/profile_remote_datasource.dart';
+import 'package:ondo/data/models/user/response/user_profile_response_model.dart';
+
+class MyProfileController extends GetxController {
+  final ProfileRemoteDatasource profileRemoteDatasource = Get.find();
+
+  final isLoading = false.obs;
+  final Rxn<UserProfileDataModel> profile = Rxn();
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadProfile();
+  }
+
+  Future<void> loadProfile() async {
+    if (isLoading.value) return;
+
+    try {
+      isLoading.value = true;
+      profile.value = await profileRemoteDatasource.getMyProfile();
+    } catch (e, s) {
+      debugPrint('Failed to load my profile: $e\n$s');
+      Get.snackbar('오류', '프로필을 불러오지 못했습니다.');
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  Future<void> logout(context) async {
+    try {
+      final localDatasource = AuthLocalDatasourceImpl();
+      await localDatasource.deleteAll();
+    } catch (e, s) {
+      debugPrint('Failed to logout: $e\n$s');
+    }
+  }
+
+  Future<bool> deleteAccount() async {
+    if (isLoading.value) return false;
+
+    try {
+      isLoading.value = true;
+      await profileRemoteDatasource.deleteAccount();
+      // 토큰 삭제
+      final localDatasource = AuthLocalDatasourceImpl();
+      await localDatasource.deleteAll();
+      return true;
+    } catch (e, s) {
+      debugPrint('Failed to delete account: $e\n$s');
+      Get.snackbar('오류', '회원탈퇴에 실패했습니다.');
+      return false;
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}

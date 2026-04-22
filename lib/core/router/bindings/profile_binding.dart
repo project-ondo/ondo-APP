480b89abe29111ca0386fb1f729f5df50e839184
heoeunseo <h.eunseo6930@gmail.com>
2026-04-22 17:38:58 +0900

    feature : ProfileBinding OtherProfileController 및 UserRemoteDatasource 의존성 추가
    OtherProfileController 등록
    UserRemoteDatasource 등록 (상대 프로필 조회에 필요)
    
    Fix: #124

--- lib/core/router/bindings/profile_binding.dart
+++ lib/core/router/bindings/profile_binding.dart
@@ -5,6 +5,7 @@ import 'package:ondo/data/datasource/auth/auth_remote_datasource.dart';
 import 'package:ondo/data/datasource/base/auth_local_datasource.dart';
 import 'package:ondo/data/datasource/media/media_remote_datasource.dart';
 import 'package:ondo/data/datasource/user/profile_remote_datasource.dart';
+import 'package:ondo/data/datasource/user/user_remote_datasource.dart';
 import 'package:ondo/data/network/clients/auth_client.dart';
 import 'package:ondo/data/repositories/auth/auth_repository_impl.dart';
 import 'package:ondo/domain/repositories/auth/auth_repository.dart';
@@ -12,27 +13,28 @@ import 'package:ondo/domain/usecases/auth/logout_usecase.dart';
 import 'package:ondo/domain/usecases/user/delete_account_usecase.dart';
 import 'package:ondo/presentation/profile/controllers/edit_profile_controller.dart';
 import 'package:ondo/presentation/profile/controllers/my_profile_controller.dart';
+import 'package:ondo/presentation/profile/controllers/other_profile_controller.dart';
 
 class ProfileBinding extends Bindings {
   @override
   void dependencies() {
     if (!Get.isRegistered<AuthLocalDatasource>()) {
       Get.lazyPut<AuthLocalDatasource>(
-            () => AuthLocalDatasourceImpl(),
+        () => AuthLocalDatasourceImpl(),
         fenix: true,
       );
     }
 
     if (!Get.isRegistered<AuthRemoteDatasource>()) {
       Get.lazyPut<AuthRemoteDatasource>(
-            () => AuthRemoteDatasource(Env.apiBaseUrl),
+        () => AuthRemoteDatasource(Env.apiBaseUrl),
         fenix: true,
       );
     }
 
     if (!Get.isRegistered<AuthClient>()) {
       Get.lazyPut<AuthClient>(
-            () => AuthClient(
+        () => AuthClient(
           localDatasource: Get.find<AuthLocalDatasource>(),
           remoteDatasource: Get.find<AuthRemoteDatasource>(),
         ),
@@ -42,7 +44,7 @@ class ProfileBinding extends Bindings {
 
     /// AuthRepository 등록 (LogoutUseCase에 필요)
     Get.lazyPut<AuthRepository>(
-          () => AuthRepositoryImpl(
+      () => AuthRepositoryImpl(
         localDatasource: Get.find<AuthLocalDatasource>(),
         remoteDatasource: Get.find<AuthRemoteDatasource>(),
       ),
@@ -50,36 +52,47 @@ class ProfileBinding extends Bindings {
 
     /// LogoutUseCase 등록
     Get.lazyPut<LogoutUseCase>(
-          () => LogoutUseCase(repository: Get.find<AuthRepository>()),
+      () => LogoutUseCase(repository: Get.find<AuthRepository>()),
     );
 
     /// 프로필 관련 datasource 등록
     Get.lazyPut<ProfileRemoteDatasource>(
-          () => ProfileRemoteDatasource(client: Get.find<AuthClient>()),
+      () => ProfileRemoteDatasource(client: Get.find<AuthClient>()),
       fenix: true,
     );
 
-    /// 미디어 업로드 datasource 등록 (이미지 변경에 필요)
+    /// 미디어 업로드/다운로드 datasource 등록
     Get.lazyPut<MediaRemoteDatasource>(
-          () => MediaRemoteDatasource(client: Get.find<AuthClient>()),
+      () => MediaRemoteDatasource(client: Get.find<AuthClient>()),
+      fenix: true,
+    );
+
+    /// UserRemoteDatasource 등록 (상대 프로필 조회에 필요)
+    Get.lazyPut<UserRemoteDatasource>(
+      () => UserRemoteDatasource(client: Get.find<AuthClient>()),
       fenix: true,
     );
 
     /// DeleteAccountUseCase 등록
     Get.lazyPut<DeleteAccountUseCase>(
-          () => DeleteAccountUseCase(
+      () => DeleteAccountUseCase(
         profileRemoteDatasource: Get.find<ProfileRemoteDatasource>(),
       ),
     );
 
     /// MyProfileController 등록
     Get.lazyPut<MyProfileController>(
-          () => MyProfileController(),
+      () => MyProfileController(),
     );
 
     /// EditProfileController 등록
     Get.lazyPut<EditProfileController>(
-          () => EditProfileController(),
+      () => EditProfileController(),
+    );
+
+    /// OtherProfileController 등록
+    Get.lazyPut<OtherProfileController>(
+      () => OtherProfileController(),
     );
   }
 }

7f9ee117b63a3a9cffb45a866d501fd0b1b9403f
user <s25013@gsm.hs.kr>
2026-04-24 13:29:00 +0900

    refactor : "BaseListRequestModel 추상화"
    커서 기반 페이지네이션 지원을 위해 BaseListRequestModel을 추상 클래스로 분리
    기존 page 기반 구현은 ListRequestModel로 이름 변경
    HomeRemoteDatasource, NotificationRemoteDatasource, UserRemoteDatasource 및
    각 RepositoryImpl에서 ListRequestModel로 타입 변경
    
    Resolve: #131

--- lib/data/datasource/home/home_remote_datasource.dart
+++ lib/data/datasource/home/home_remote_datasource.dart
@@ -9,7 +9,7 @@ class HomeRemoteDatasource {
 
   HomeRemoteDatasource({required this.client});
 
-  Future<Map?> loadRecommendPostList(BaseListRequestModel model) async {
+  Future<Map?> loadRecommendPostList(ListRequestModel model) async {
     final log = ApiConstants(logName: "홈 추천 게시물 조회");
 
     try {

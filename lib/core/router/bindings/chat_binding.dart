caabffe1069ac4f1b0c66cdaf9200d8e2e870e82
허은서 <heoeunseong140@gmail.com>
2026-05-14 23:54:13 +0900

    "feature : WebSocket STOMP 연결/해제 구현" (#172)
    - StompFrame: STOMP 프레임 직렬화/역직렬화 유틸 추가
    - ChatStompClient: web_socket_channel 기반 STOMP 클라이언트 구현
      - JWT Authorization 헤더 포함 CONNECT 처리
      - 구독(subscribe) / 발행(publish) / 해제(unsubscribe) 지원
      - 연결 실패 시 자동 재연결 (3초 간격, 최대 5회)
      - 앱 포그라운드/백그라운드에 따른 Ping 일시 중단/재개 (30초 주기)
    - ChatBinding: ChatStompClient 싱글톤 DI 등록 (fenix: true)
    
    Resolve: #161

--- lib/core/router/bindings/chat_binding.dart
+++ lib/core/router/bindings/chat_binding.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:ondo/data/datasource/chat/chat_remote_datasource.dart';
+import 'package:ondo/data/network/websocket/chat_stomp_client.dart';
 import 'package:ondo/data/repositories/chat/chat_repository_impl.dart';
 import 'package:ondo/domain/usecases/chat/block_chat_room_use_case.dart';
 import 'package:ondo/domain/usecases/chat/cancel_block_chat_room_use_case.dart';
@@ -8,11 +9,19 @@ import 'package:ondo/domain/usecases/chat/load_my_chat_room_list_use_case.dart';
 import 'package:ondo/domain/usecases/chat/read_chat_message_use_case.dart';
 import 'package:ondo/presentation/chat/controllers/chat_main_controller.dart';
 
+import '../../../data/datasource/base/auth_local_datasource.dart';
 import '../../../data/network/clients/auth_client.dart';
 
 class ChatBinding extends Bindings {
   @override
   void dependencies() {
+    /// STOMP 클라이언트 싱글톤 등록
+    /// fenix: true → 화면 이탈 후에도 인스턴스 유지/재생성 가능
+    Get.lazyPut<ChatStompClient>(
+      () => ChatStompClient(localDatasource: Get.find<AuthLocalDatasource>()),
+      fenix: true,
+    );
+
     ///chat 관련 dataSource 등록
     Get.lazyPut<ChatRemoteDatasource>(
       () => ChatRemoteDatasource(client: Get.find<AuthClient>()),

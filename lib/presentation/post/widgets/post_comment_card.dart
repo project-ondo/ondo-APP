74c35c3b013eae2e7cb43e001c0538ff3c2aa492
류수연 <s25010@gsm.hs.kr>
2026-05-19 19:53:51 +0900

    Revert "[FEATURE}  댓글 생성, 댓글 조회, 댓글 삭제 api 연결"

--- lib/presentation/post/widgets/post_comment_card.dart
+++ lib/presentation/post/widgets/post_comment_card.dart
@@ -4,7 +4,6 @@ import 'package:ondo/core/design_system/app_colors.dart';
 import 'package:ondo/core/design_system/app_layout.dart';
 import 'package:ondo/core/design_system/app_text_styles.dart';
 import 'package:ondo/core/design_system/components/custom_profile_circle.dart';
-import 'package:ondo/data/models/comment/response/comment_model.dart';
 import 'package:ondo/presentation/post/controllers/post_view_controller.dart';
 
 class PostCommentCard extends GetView<PostViewController> {
@@ -13,7 +12,7 @@ class PostCommentCard extends GetView<PostViewController> {
     required this.comment,
   });
 
-  final CommentModel comment;
+  final Comment comment;
 
   @override
   Widget build(BuildContext context) {
@@ -26,16 +25,16 @@ class PostCommentCard extends GetView<PostViewController> {
         AppGap.h12,
         Expanded(child: _body()),
         AppGap.h6,
-        // TODO: 로그인 유저 id와 비교해서 본인 댓글만 삭제 버튼 노출
-        GestureDetector(
-          onTap: () {
-            controller.deleteComment(comment);
-          },
-          child: Text(
-            "삭제",
-            style: AppTextStyles.caption(textColor: AppColors.red),
+        if (comment.isMy)
+          GestureDetector(
+            onTap: () {
+              controller.deleteComment(comment);
+            },
+            child: Text(
+              "삭제",
+              style: AppTextStyles.caption(textColor: AppColors.red),
+            ),
           ),
-        ),
       ],
     );
   }
@@ -45,12 +44,12 @@ class PostCommentCard extends GetView<PostViewController> {
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
       Text(
-        comment.author ?? '',
+        comment.author,
         style: AppTextStyles.caption(textColor: AppColors.gray60),
       ),
       AppGap.v6,
       Text(
-        comment.content,
+        comment.comment,
         style: AppTextStyles.caption(textColor: AppColors.gray90),
         maxLines: 2,
         overflow: TextOverflow.ellipsis,

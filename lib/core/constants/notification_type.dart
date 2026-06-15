2014ca634af614c959991ea01c01fa2a064d8409
eum108 <s25013@gsm.hs.kr>
2026-06-15 20:43:13 +0900

    refactor : "알림 type을 String에서 NotificationType enum으로 통일"
    서버 알림 타입을 매직 스트링 대신 NotificationType enum으로 관리.
    NotificationModel.type, NotificationEntity.type을 String에서
    NotificationType으로 변경하고, fromJson/toJson에서 enum value로
    매핑. 기존 presentation 레이어의 NotificationState enum은
    제거하고 core/constants의 NotificationType으로 대체.

---
+++ lib/core/constants/notification_type.dart
@@ -0,0 +1,17 @@
+enum NotificationType {
+  chatMessage('CHAT_MESSAGE'),
+  matchCreated('MATCH_CREATED'),
+  postComment('POST_COMMENT'),
+  rateRequest('RATE_REQUEST'),
+  postLike('POST_LIKE'),
+  chatMemberLeft('CHAT_MEMBER_LEFT'),
+  reportReceived('REPORT_RECEIVED'),
+  reportResolved('REPORT_RESOLVED'),
+  reportContentDeleted('REPORT_CONTENT_DELETED'),
+  userSuspended('USER_SUSPENDED')
+  ;
+
+  final String value;
+
+  const NotificationType(this.value);
+}
